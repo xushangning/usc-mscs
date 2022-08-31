cf31485ec474f2d26377741d9a87625023ed0930
Shining Xu <shangnin@usc.edu>
2022-08-30 21:03:17 -0700

    CSCI 580: HW 2: add code from HW 1

--- CSCI 580/hw/2/rend.cpp
+++ CSCI 580/hw/2/rend.cpp
@@ -1,11 +1,25 @@
 #include	"stdafx.h"
 #include	"stdio.h"
 #include	"math.h"
+#include	<algorithm>
 #include	"Gz.h"
 #include	"rend.h"
 
 /***********************************************/
 /* HW1 methods: copy here the methods from HW1 */
+
+constexpr int N_RGB = 3;
+
+constexpr GzPixel BACKGROUND{ 0, 0, 1000, 1, 0 };
+
+constexpr GzIntensity MAX_INTENSITY = 4095;
+
+inline char twelve2EightBitColor(GzIntensity color)
+{
+	return static_cast<char>(color >> 4);
+}
+
+using std::min;
 
 GzRender::GzRender(int xRes, int yRes)
 {
@@ -14,18 +28,26 @@ GzRender::GzRender(int xRes, int yRes)
  -- allocate memory for framebuffer : 3 bytes(b, g, r) x width x height
  -- allocate memory for pixel buffer
  */
+	xres = min(xRes, MAXXRES);
+	yres = min(yRes, MAXYRES);
+
+	auto resolution = xres * yres;
+	framebuffer = new char[resolution * N_RGB];
+	pixelbuffer = new GzPixel[resolution];
 }
 
 GzRender::~GzRender()
 {
 /* HW1.2 clean up, free buffer memory */
-
+	delete pixelbuffer;
+	delete framebuffer;
 }
 
 int GzRender::GzDefault()
 {
 /* HW1.3 set pixel buffer to some default values - start a new frame */
-
+	for (int i = 0; i < xres * yres; ++i)
+		pixelbuffer[i] = BACKGROUND;
 	return GZ_SUCCESS;
 }
 
@@ -33,7 +55,15 @@ int GzRender::GzDefault()
 int GzRender::GzPut(int i, int j, GzIntensity r, GzIntensity g, GzIntensity b, GzIntensity a, GzDepth z)
 {
 /* HW1.4 write pixel values into the buffer */
+	if (!(i >= 0 && j >= 0 && i < xres && j < yres))
+		return GZ_FAILURE;
 
+	pixelbuffer[ARRAY(i, j)] = {
+		min(r, MAX_INTENSITY),
+		min(g, MAX_INTENSITY),
+		min(b, MAX_INTENSITY),
+		a, z
+	};
 	return GZ_SUCCESS;
 }
 
@@ -41,7 +71,20 @@ int GzRender::GzPut(int i, int j, GzIntensity r, GzIntensity g, GzIntensity b, G
 int GzRender::GzGet(int i, int j, GzIntensity *r, GzIntensity *g, GzIntensity *b, GzIntensity *a, GzDepth *z)
 {
 /* HW1.5 retrieve a pixel information from the pixel buffer */
+	if (!(i >= 0 && j >= 0 && i < xres && j < yres))
+		return GZ_FAILURE;
 
+	auto& pixel = pixelbuffer[ARRAY(i, j)];
+	if (r)
+		*r = pixel.red;
+	if (g)
+		*g = pixel.green;
+	if (b)
+		*b = pixel.blue;
+	if (a)
+		*a = pixel.alpha;
+	if (z)
+		*z = pixel.z;
 	return GZ_SUCCESS;
 }
 
@@ -49,7 +92,13 @@ int GzRender::GzGet(int i, int j, GzIntensity *r, GzIntensity *g, GzIntensity *b
 int GzRender::GzFlushDisplay2File(FILE* outfile)
 {
 /* HW1.6 write image to ppm file -- "P6 %d %d 255\r" */
-	
+	fprintf(outfile, "P6 %d %d 255\n", xres, yres);		// Header
+
+	for (int i = 0; i < xres * yres; ++i) {
+		fputc(twelve2EightBitColor(pixelbuffer[i].red), outfile);
+		fputc(twelve2EightBitColor(pixelbuffer[i].green), outfile);
+		fputc(twelve2EightBitColor(pixelbuffer[i].blue), outfile);
+	}
 	return GZ_SUCCESS;
 }
 
@@ -60,7 +109,11 @@ int GzRender::GzFlushDisplay2FrameBuffer()
 	- CAUTION: when storing the pixels into the frame buffer, the order is blue, green, and red
 	- NOT red, green, and blue !!!
 */
-
+	for (int i = 0, j = 0; i < xres * yres; ++i, j += N_RGB) {
+		framebuffer[j] = twelve2EightBitColor(pixelbuffer[i].blue);
+		framebuffer[j + 1] = twelve2EightBitColor(pixelbuffer[i].green);
+		framebuffer[j + 2] = twelve2EightBitColor(pixelbuffer[i].red);
+	}
 	return GZ_SUCCESS;
 }
 
