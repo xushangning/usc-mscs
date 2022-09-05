9d6bc1b86c8ee06ac94d4ca74e4bb3b5b5e7df8e
Shining Xu <shangnin@usc.edu>
2022-09-05 16:35:43 -0700

    CSCI 580: HW 2: test z index interpolation

--- CSCI 580/hw/UnitTests/UnitTests.cpp
+++ CSCI 580/hw/UnitTests/UnitTests.cpp
@@ -12,6 +12,8 @@ namespace UnitTests
     {
         GzRender render_{ 128, 128 };
 
+        typedef GzCoord Triangle[3];
+
         static const GzColor kColor;
 
     public:
@@ -26,6 +28,29 @@ namespace UnitTests
         TEST_METHOD_INITIALIZE(InitializeMethod)
         {
             render_.GzDefault();
+        }
+
+        TEST_METHOD(TestZIndexInterpolation)
+        {
+            constexpr int C = 4;
+            // a triangle formed by the intersection of the surface
+            // x + y + z = C and the first octant
+            Triangle triangle{ {C, 0, 0}, {0, C, 0}, {0, 0, C} };
+            auto argument_type = GZ_POSITION;
+            void* argument = &triangle;
+            render_.GzPutTriangle(1, &argument_type, &argument);
+
+            // x and y start at 1 to avoid the triangle's sides.
+            for (int x = 1; x < C; ++x)
+                for (int y = 1; y < C - x; ++y) {
+                    GzPixel pixel;
+                    render_.GzGet(x, y, &pixel.red, &pixel.green, &pixel.blue, &pixel.alpha, &pixel.z);
+
+                    Assert::AreEqual(render_.ctoi(kColor[0]), pixel.red);
+                    Assert::AreEqual(render_.ctoi(kColor[1]), pixel.green);
+                    Assert::AreEqual(render_.ctoi(kColor[2]), pixel.blue);
+                    Assert::AreEqual(C - x - y, pixel.z);
+                }
         }
     };
 
