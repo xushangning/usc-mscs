541dc759674d5cc02a3ee06e41c0b99e6ffe25d1
Shining Xu <shangnin@usc.edu>
2022-09-14 13:56:50 -0700

    CSCI 580: HW 3: add code from HW 2

--- CSCI 580/hw/3/rend.cpp
+++ CSCI 580/hw/3/rend.cpp
@@ -1,12 +1,22 @@
 /* CS580 Homework 3 */
 
-#include	"stdafx.h"
 #include	"stdio.h"
 #include	"math.h"
+#include	<numeric>
+#include	<algorithm>
+#include	<array>
 #include	"Gz.h"
 #include	"rend.h"
 
+import dda;
+
 #define PI (float) 3.14159265358979323846
+
+constexpr int N_RGB = 3;
+
+constexpr GzPixel BACKGROUND{ 0, 0, 1000, 1, std::numeric_limits<GzDepth>::max() };
+
+constexpr GzIntensity MAX_INTENSITY = 4095;
 
 int GzRender::GzRotXMat(float degree, GzMatrix mat)
 {
@@ -60,6 +70,8 @@ int GzRender::GzScaleMat(GzCoord scale, GzMatrix mat)
 }
 
 
+using std::min;
+
 GzRender::GzRender(int xRes, int yRes)
 {
 /* HW1.1 create a framebuffer for MS Windows display:
@@ -67,7 +79,12 @@ GzRender::GzRender(int xRes, int yRes)
  -- allocate memory for framebuffer : 3 bytes(b, g, r) x width x height
  -- allocate memory for pixel buffer
  */
-	framebuffer = (char*) malloc (3 * sizeof(char) * xRes * yRes);
+	xres = min(xRes, MAXXRES);
+	yres = min(yRes, MAXYRES);
+
+	auto resolution = xres * yres;
+	framebuffer = new char[resolution * N_RGB];
+	pixelbuffer = new GzPixel[resolution];
 
 /* HW 3.6
 - setup Xsp and anything only done once 
@@ -78,13 +95,15 @@ GzRender::GzRender(int xRes, int yRes)
 GzRender::~GzRender()
 {
 /* HW1.2 clean up, free buffer memory */
-
+	delete pixelbuffer;
+	delete framebuffer;
 }
 
 int GzRender::GzDefault()
 {
 /* HW1.3 set pixel buffer to some default values - start a new frame */
-
+	for (int i = 0; i < xres * yres; ++i)
+		pixelbuffer[i] = BACKGROUND;
 	return GZ_SUCCESS;
 }
 
@@ -132,7 +151,16 @@ int GzRender::GzPopMatrix()
 int GzRender::GzPut(int i, int j, GzIntensity r, GzIntensity g, GzIntensity b, GzIntensity a, GzDepth z)
 {
 /* HW1.4 write pixel values into the buffer */
+	if (!(i >= 0 && j >= 0 && i < xres && j < yres))
+		return GZ_FAILURE;
 
+	if (z < pixelbuffer[ARRAY(i, j)].z)
+		pixelbuffer[ARRAY(i, j)] = {
+			min(r, MAX_INTENSITY),
+			min(g, MAX_INTENSITY),
+			min(b, MAX_INTENSITY),
+			a, z
+		};
 	return GZ_SUCCESS;
 }
 
@@ -140,15 +168,39 @@ int GzRender::GzPut(int i, int j, GzIntensity r, GzIntensity g, GzIntensity b, G
 int GzRender::GzGet(int i, int j, GzIntensity *r, GzIntensity *g, GzIntensity *b, GzIntensity *a, GzDepth *z)
 {
 /* HW1.5 retrieve a pixel information from the pixel buffer */
+	if (!(i >= 0 && j >= 0 && i < xres && j < yres))
+		return GZ_FAILURE;
 
+	auto& pixel = pixelbuffer[ARRAY(i, j)];
+	if (r)
+		*r = pixel.red;
+	if (g)
+		*g = pixel.green;
+	if (b)
+		*b = pixel.blue;
+	if (a)
+		*a = pixel.alpha;
+	if (z)
+		*z = pixel.z;
 	return GZ_SUCCESS;
 }
 
 
+inline char twelve2EightBitColor(GzIntensity color)
+{
+	return static_cast<char>(color >> 4);
+}
+
 int GzRender::GzFlushDisplay2File(FILE* outfile)
 {
 /* HW1.6 write image to ppm file -- "P6 %d %d 255\r" */
-	
+	fprintf(outfile, "P6 %d %d 255\n", xres, yres);		// Header
+
+	for (int i = 0; i < xres * yres; ++i) {
+		fputc(twelve2EightBitColor(pixelbuffer[i].red), outfile);
+		fputc(twelve2EightBitColor(pixelbuffer[i].green), outfile);
+		fputc(twelve2EightBitColor(pixelbuffer[i].blue), outfile);
+	}
 	return GZ_SUCCESS;
 }
 
@@ -159,7 +211,11 @@ int GzRender::GzFlushDisplay2FrameBuffer()
 	- CAUTION: when storing the pixels into the frame buffer, the order is blue, green, and red 
 	- NOT red, green, and blue !!!
 */
-
+	for (int i = 0, j = 0; i < xres * yres; ++i, j += N_RGB) {
+		framebuffer[j] = twelve2EightBitColor(pixelbuffer[i].blue);
+		framebuffer[j + 1] = twelve2EightBitColor(pixelbuffer[i].green);
+		framebuffer[j + 2] = twelve2EightBitColor(pixelbuffer[i].red);
+	}
 	return GZ_SUCCESS;
 }
 
@@ -173,8 +229,58 @@ int GzRender::GzPutAttribute(int numAttributes, GzToken	*nameList, GzPointer *va
 -- Set renderer attribute states (e.g.: GZ_RGB_COLOR default color)
 -- In later homeworks set shaders, interpolaters, texture maps, and lights
 */
-
+	for (int i = 0; i < numAttributes; ++i)
+		switch (nameList[i]) {
+		case GZ_RGB_COLOR:
+			std::copy_n(*static_cast<GzColor*>(valueList[i]), N_RGB, flatcolor);
+			break;
+		}
 	return GZ_SUCCESS;
+}
+
+inline float crossProduct(const GzCoord& tail, const GzCoord& head_start, const GzCoord& head_end)
+{
+	return (head_start[0] - tail[0]) * (head_end[1] - tail[1]) - (head_end[0] - tail[0]) * (head_start[1] - tail[1]);
+}
+
+/// <summary>
+/// Rasterize a trapezoid parallel to the x-axis or its degenerated form, a
+/// triangle with one edge parallel to the x-axis.
+/// </summary>
+/// 
+/// <param name="leader">
+/// An iterator that provides coordinates along one side that is not parallel
+/// to the x-axis. It is the iterator used to determine the end of a loop.
+/// </param>
+/// <param name="follower">
+/// An iterator that provides coordinates along the other side that is not
+/// parallel to the x-axis
+/// </param>
+/// <param name="leader_on_x_begin">
+/// Whether the leader iterator corresponds to the side with smaller
+/// x-coordinates than the other.
+/// </param>
+void putTrapezoid(
+	GzRender& render, const GzIntensity(&color)[3],
+	DDA::iterator& leader, DDA::iterator& follower, const DDA::iterator& end,
+	bool leader_on_x_begin
+)
+{
+	auto& x_begin = leader_on_x_begin ? leader : follower,
+		& x_end = leader_on_x_begin ? follower : leader;
+	while (leader != end) {
+		DDA x(0, *x_begin, *x_end);		// DDA along the x axis.
+		for (auto& coord : x)
+			render.GzPut(
+				static_cast<int>(coord[0]), static_cast<int>(coord[1]),
+				color[0], color[1], color[2],
+				1, static_cast<GzDepth>(coord[2])
+			);
+
+		++leader;
+		++follower;
+	}
+
 }
 
 int GzRender::GzPutTriangle(int numParts, GzToken *nameList, GzPointer *valueList)
@@ -182,12 +288,60 @@ int GzRender::GzPutTriangle(int numParts, GzToken *nameList, GzPointer *valueLis
 {
 /* HW 2.2
 -- Pass in a triangle description with tokens and values corresponding to
-      GZ_NULL_TOKEN:		do nothing - no values
-      GZ_POSITION:		3 vert positions in model space
+	GZ_NULL_TOKEN:		do nothing - no values
+	GZ_POSITION:		3 vert positions in model space
 -- Invoke the rastrizer/scanline framework
 -- Return error code
 */
+	for (int i = 0; i < numParts; ++i)
+		switch (nameList[i]) {
+		case GZ_POSITION: {
+			auto first_vertex = static_cast<GzCoord*>(valueList[i]);
 
+			std::array vertices = { first_vertex, first_vertex + 1, first_vertex + 2 };
+			// Sort by y.
+			std::sort(vertices.begin(), vertices.end(), [](GzCoord* a, GzCoord* b) { return (*a)[1] < (*b)[1]; });
+			GzCoord& y_begin = *vertices[0],
+				/// <summary>
+				/// The vertex with the middling y-coordinate among the three.
+				/// The triangle is divided into two parts by the line that is
+				/// parallel to the x-ais and goes through this vertex.
+				/// </summary>
+				& y_mid = *vertices[1],
+				& y_end = *vertices[2];
+
+			auto cross_product = crossProduct(y_begin, y_end, y_mid);
+			if (cross_product == 0)
+				// degenerated triangle
+				break;
+			// Whether the edges (y_begin, y_mid, y_end) have smaller
+			// x-coordinates than the edge (y_begin, y_end) i.e., on the left
+			// of (y_begin, y_end).
+			bool y_mid_on_x_begin = cross_product > 0;
+
+			GzIntensity color[]{ ctoi(flatcolor[0]), ctoi(flatcolor[1]), ctoi(flatcolor[2]) };
+
+			DDA begin_end(1, y_begin, y_end);
+			auto begin_end_iter = begin_end.begin();
+			if (y_begin[1] != y_mid[1]) {
+				// Rasterize the part of the triangle with smaller x-coordinates.
+				DDA begin_mid(1, y_begin, y_mid);
+				auto begin_mid_iter = begin_mid.begin();
+
+				putTrapezoid(*this, color, begin_mid_iter, begin_end_iter, begin_mid.end(), y_mid_on_x_begin);
+			}
+
+			if (y_mid[1] != y_end[1]) {
+				// Rasterize the other part of the triangle with larger x-coordinates.
+				DDA mid_end(1, y_mid, y_end);
+				auto mid_end_iter = mid_end.begin();
+
+				putTrapezoid(*this, color, mid_end_iter, begin_end_iter, mid_end.end(), y_mid_on_x_begin);
+			}
+
+			break;
+		}
+		}
 	return GZ_SUCCESS;
 }
 
